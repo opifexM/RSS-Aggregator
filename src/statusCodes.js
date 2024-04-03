5e72e92739caaa28365e8986b913fff247d31c4f
opifex <opifexM@outlook.com>
2024-04-03 10:06:44 -0400

    Replace Status import from rss-repository.js with statusCodes.js

---
+++ src/statusCodes.js
@@ -0,0 +1,11 @@
+const Status = {
+  READY: 'Ready',
+  PROCESS: 'Process',
+  VALIDATION_ERROR: 'URL validation error',
+  CONNECTION_ERROR: 'URL connection error',
+  RSS_EXISTS_ERROR: 'RSS already exists',
+  RSS_PARSE_ERROR: 'RSS parsing error',
+  FINISHED: 'URL processed',
+};
+
+export default Status;
